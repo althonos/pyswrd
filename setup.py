8c4f4267de89f16dfac7682d6ea7f5f65be4e065
Martin Larralde <martin.larralde@embl.de>
2024-01-13 11:25:05 +0100

    Add `setuptools` files to build the Cython extension

---
+++ setup.py
@@ -0,0 +1,288 @@
+import configparser
+import functools
+import glob
+import itertools
+import io
+import multiprocessing.pool
+import os
+import platform
+import re
+import setuptools
+import subprocess
+import string
+import sys
+import sysconfig
+from distutils.command.clean import clean as _clean
+from distutils.errors import CompileError
+from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.sdist import sdist as _sdist
+from setuptools.extension import Extension
+
+try:
+    from Cython.Build import cythonize
+except ImportError as err:
+    cythonize = err
+
+# --- Utils ------------------------------------------------------------------
+
+_HEADER_PATTERN = re.compile("^@@ -(\d+),?(\d+)? \+(\d+),?(\d+)? @@$")
+
+
+def _eprint(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
+
+def _patch_osx_compiler(compiler):
+    # On newer OSX, Python has been compiled as a universal binary, so
+    # it will attempt to pass universal binary flags when building the
+    # extension. This will not work because the code makes use of SSE2.
+    for tool in ("compiler", "compiler_so", "linker_so"):
+        flags = getattr(compiler, tool)
+        i = next(
+            (
+                i
+                for i in range(1, len(flags))
+                if flags[i - 1] == "-arch" and flags[i] != platform.machine()
+            ),
+            None,
+        )
+        if i is not None:
+            flags.pop(i)
+            flags.pop(i - 1)
+
+
+def _detect_target_machine(platform):
+    if platform == "win32":
+        return "x86"
+    return platform.rsplit("-", 1)[-1]
+
+
+def _detect_target_cpu(platform):
+    machine = _detect_target_machine(platform)
+    if re.match("^mips", machine):
+        return "mips"
+    elif re.match("^(aarch64|arm64)$", machine):
+        return "aarch64"
+    elif re.match("^arm", machine):
+        return "arm"
+    elif re.match("(x86_64)|AMD64|amd64", machine):
+        return "x86_64"
+    elif re.match("(x86)|(^i.86$)", machine):
+        return "x86"
+    elif re.match("^(powerpc|ppc)", machine):
+        return "ppc"
+    return None
+
+
+def _detect_target_system(platform):
+    if platform.startswith("win"):
+        return "windows"
+    elif platform.startswith("macos"):
+        return "macos"
+    elif platform.startswith("linux"):
+        return "linux_or_android"
+    elif platform.startswith("freebsd"):
+        return "freebsd"
+    return None
+
+
+# --- Commands ------------------------------------------------------------------
+
+
+class sdist(_sdist):
+    """A `sdist` that generates a `pyproject.toml` on the fly."""
+
+    def run(self):
+        # build `pyproject.toml` from `setup.cfg`
+        c = configparser.ConfigParser()
+        c.add_section("build-system")
+        c.set("build-system", "requires", str(self.distribution.setup_requires))
+        c.set("build-system", "build-backend", '"setuptools.build_meta"')
+        with open("pyproject.toml", "w") as pyproject:
+            c.write(pyproject)
+        # run the rest of the packaging
+        _sdist.run(self)
+
+
+class build_ext(_build_ext):
+    """A `build_ext` that adds various SIMD flags and defines."""
+
+    def initialize_options(self):
+        _build_ext.initialize_options(self)
+        self.target_machine = None
+        self.target_system = None
+        self.target_cpu = None
+
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # check platform
+        if self.plat_name is None:
+            self.plat_name = sysconfig.get_platform()
+        # detect platform options
+        self.target_machine = _detect_target_machine(self.plat_name)
+        self.target_system = _detect_target_system(self.plat_name)
+        self.target_cpu = _detect_target_cpu(self.plat_name)
+
+    def _check_getid(self):
+        _eprint('checking whether `PyInterpreterState_GetID` is available')
+
+        base = "have_getid"
+        testfile = os.path.join(self.build_temp, "{}.c".format(base))
+        objects = []
+
+        self.mkpath(self.build_temp)
+        with open(testfile, "w") as f:
+            f.write("""
+            #include <stdint.h>
+            #include <stdlib.h>
+            #include <Python.h>
+
+            int main(int argc, char *argv[]) {{
+                PyInterpreterState_GetID(NULL);
+                return 0;
+            }}
+            """)
+
+        if self.compiler.compiler_type == "msvc":
+            flags = ["/WX"]
+        else:
+            flags = ["-Werror=implicit-function-declaration"]
+
+        try:
+            self.mkpath(self.build_temp)
+            objects = self.compiler.compile([testfile], extra_postargs=flags)
+        except CompileError:
+            _eprint("no")
+            return False
+        else:
+            _eprint("yes")
+            return True
+        finally:
+            os.remove(testfile)
+            for obj in filter(os.path.isfile, objects):
+                os.remove(obj)
+
+    def build_extension(self, ext):
+        # show the compiler being used
+        _eprint("building", ext.name, "with", self.compiler.compiler_type, "compiler for platform", self.plat_name)
+
+        # add debug symbols if we are building in debug mode
+        if self.debug:
+            if self.compiler.compiler_type in {"unix", "cygwin", "mingw32"}:
+                ext.extra_compile_args.append("-g")
+            elif self.compiler.compiler_type == "msvc":
+                ext.extra_compile_args.append("/Z7")
+            if sys.implementation.name == "cpython":
+                ext.define_macros.append(("CYTHON_TRACE_NOGIL", 1))
+        else:
+            ext.define_macros.append(("CYTHON_WITHOUT_ASSERTIONS", 1))
+
+        # check if `PyInterpreterState_GetID` is defined
+        if self._check_getid():
+            ext.define_macros.append(("HAS_PYINTERPRETERSTATE_GETID", 1))
+
+        # build the rest of the extension as normal
+        ext._needs_stub = False
+        _build_ext.build_extension(self, ext)
+
+    def build_extensions(self):
+        # check `cythonize` is available
+        if isinstance(cythonize, ImportError):
+            raise RuntimeError(
+                "Cython is required to run `build_ext` command"
+            ) from cythonize
+
+        # remove universal compilation flags for OSX
+        if platform.system() == "Darwin":
+            _patch_osx_compiler(self.compiler)
+
+        # use debug directives with Cython if building in debug mode
+        cython_args = {
+            "include_path": ["include"],
+            "compiler_directives": {
+                "cdivision": True,
+                "nonecheck": False,
+            },
+            "compile_time_env": {
+                "SYS_IMPLEMENTATION_NAME": sys.implementation.name,
+                "SYS_VERSION_INFO_MAJOR": sys.version_info.major,
+                "SYS_VERSION_INFO_MINOR": sys.version_info.minor,
+                "SYS_VERSION_INFO_MICRO": sys.version_info.micro,
+                "DEFAULT_BUFFER_SIZE": io.DEFAULT_BUFFER_SIZE,
+                "TARGET_CPU": self.target_cpu,
+                "TARGET_SYSTEM": self.target_system,
+            },
+        }
+        if self.force:
+            cython_args["force"] = True
+        if self.debug:
+            cython_args["annotate"] = True
+            cython_args["compiler_directives"]["cdivision_warnings"] = True
+            cython_args["compiler_directives"]["warn.undeclared"] = True
+            cython_args["compiler_directives"]["warn.unreachable"] = True
+            cython_args["compiler_directives"]["warn.maybe_uninitialized"] = True
+            cython_args["compiler_directives"]["warn.unused"] = True
+            cython_args["compiler_directives"]["warn.unused_arg"] = True
+            cython_args["compiler_directives"]["warn.unused_result"] = True
+            cython_args["compiler_directives"]["warn.multiple_declarators"] = True
+        else:
+            cython_args["compiler_directives"]["boundscheck"] = False
+            cython_args["compiler_directives"]["wraparound"] = False
+
+        # cythonize the extensions
+        self.extensions = cythonize(self.extensions, **cython_args)
+
+        # build the extensions as normal
+        _build_ext.build_extensions(self)
+
+
+class clean(_clean):
+    """A `clean` that removes intermediate files created by Cython."""
+
+    def run(self):
+
+        source_dir = os.path.join(os.path.dirname(__file__), "pyswrd")
+
+        patterns = ["*.html"]
+        if self.all:
+            patterns.extend(["*.so", "*.c", "*.cpp"])
+
+        for pattern in patterns:
+            for file in glob.glob(os.path.join(source_dir, pattern)):
+                _eprint("removing {!r}".format(file))
+                os.remove(file)
+
+        _clean.run(self)
+
+
+# --- Setup ---------------------------------------------------------------------
+
+setuptools.setup(
+    ext_modules=[
+        Extension(
+            "pyswrd._sword",
+            language="c++",
+            sources=[
+                os.path.join("vendor", "sword", "src", "chain.cpp"),
+                os.path.join("vendor", "sword", "src", "evalue.cpp"),
+                os.path.join("vendor", "sword", "src", "hash.cpp"),
+                os.path.join("vendor", "sword", "src", "kmers.cpp"),
+                os.path.join("vendor", "sword", "src", "reader.cpp"),
+                os.path.join("vendor", "sword", "src", "score_matrix.cpp"),
+                os.path.join("vendor", "sword", "src", "utils.cpp"),
+                os.path.join("pyswrd", "_sword.pyx"),
+            ],
+            extra_compile_args=[],
+            include_dirs=[
+                os.path.join("vendor", "sword", "src"),
+                "pyswrd",
+                "include",
+            ],
+        ),
+    ],
+    cmdclass={
+        "sdist": sdist,
+        "build_ext": build_ext,
+        "clean": clean,
+    },
+)
